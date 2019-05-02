89f606a344fffa23d81375e384368755eaa7f083
asaffeldman <assaf.feldman1@mail.huji.ac.il>
2019-05-02 21:25:19 +0300

    commit for after game group stats screen

--- app/src/main/java/com/example/dontouchv1/EndGamePersonalStat.java
+++ app/src/main/java/com/example/dontouchv1/EndGamePersonalStat.java
@@ -1,11 +1,13 @@
 package com.example.dontouchv1;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 
 /**
@@ -25,5 +27,8 @@ public class EndGamePersonalStat extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_end_game_personal_stat, container, false);
     }
+
+
+
 
 }
